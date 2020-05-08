10862c813e0771bb84778d3f12b365ae6cc0fc5a
MM-N <mirren91@hotmail.com>
2020-05-08 11:39:27 +0100

    Helper class

---
+++ Utilities/Helpers.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020 GCVRS, Inc. All Rights Reserved 
+
+
+#include "Helpers.h"
+
+Helpers::Helpers()
+{
+}
+
+Helpers::~Helpers()
+{
+}
---
+++ Utilities/Helpers.h
@@ -0,0 +1,45 @@
+// Copyright 2020 GCVRS, Inc. All Rights Reserved 
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AUDIODEMO_API Helpers
+{
+public:
+	Helpers();
+
+	class UTextUtil {
+	public:
+
+		/** Gets corresponding enum value given the enum to search and the string name**/
+		template <typename EnumType>
+		static FORCEINLINE EnumType GetEnumValueFromString(const FString& EnumName, const FString& String)
+		{
+			UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, *EnumName, true);
+			if (!Enum)
+			{
+				return EnumType(0);
+			}
+			return (EnumType)Enum->GetIndexByName(FName(*String));
+		}
+
+		/** Gets corresponding string value of a given enum **/
+		template<typename TEnum>
+		static FORCEINLINE FString GetEnumValueAsString(const FString& Name, TEnum Value)
+		{
+			const UEnum* enumPtr = FindObject<UEnum>(ANY_PACKAGE, *Name, true);
+			if (!enumPtr)
+			{
+				return FString("Invalid");
+			}
+			return enumPtr->GetValueByName((int64)Value).ToString();
+		}
+	};
+
+
+	~Helpers();
+};
