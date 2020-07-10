3f6de80f1015b0737bbf4d4eeb015c3a8c62bab1
MM-N <mirren91@hotmail.com>
2020-07-10 16:49:56 +0100

    Update CustomShader.h

--- Shaders/CustomShader.h
+++ Shaders/CustomShader.h
@@ -5,6 +5,8 @@
 
 //Class name: MyUE4ProjectNameModule
 
+//To use custom HLSL shader code you need to use custom startup module in UE4
+
 class FMyUE4ProjectModule
 	/* only IModuleInterface necessary if not hosting gamemode in this module */
 	: public FDefaultGameModuleImpl
